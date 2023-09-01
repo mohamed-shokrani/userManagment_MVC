734bf4032cff631bfbee22ed44a0fe59188c721f
mohamed-shokrani <mohamedshokrani55@gmail.com>
2023-09-01 21:42:55 +0300

    Add project files.

---
+++ userManagment/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using userManagment.Models;
+
+namespace userManagment.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ApplicationUser>().ToTable("Users","security");
+            modelBuilder.Entity<IdentityRole>().ToTable("Roles" , "security");
+            modelBuilder.Entity<IdentityUserRole<string>>().ToTable("UserRoles", "security");
+            modelBuilder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins", "security");
+
+            modelBuilder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims" , "security");
+            modelBuilder.Entity<IdentityUserToken<string>>().ToTable("UserTokens" , "security");
+
+        }
+    }
+}
---
+++ userManagment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace userManagment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
